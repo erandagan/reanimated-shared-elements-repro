624e98636e3a3cc408c4b0dd8c63071add5142f1
Eran Dagan <eran@botika.io>
2024-07-23 11:29:56 +0300

    Reproduction of a SET bug in modals

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  NavigationContainer,
+  StackActions,
+  useNavigation,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import Animated from "react-native-reanimated";
 
-export default function App() {
+function HomeScreen() {
+  const nav = useNavigation();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+
+      <Animated.Image
+        sharedTransitionTag="image"
+        source={{ uri: "https://picsum.photos/200/300" }}
+        style={{ width: 100, height: 100 }}
+      />
+      <Button title="Open Modal" onPress={() => nav.navigate("Modal")} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ModalScreen() {
+  const nav = useNavigation();
+  return (
+    <View style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.3)" }}>
+      <Animated.Image
+        sharedTransitionTag="image"
+        source={{ uri: "https://picsum.photos/200/300" }}
+        style={{ width: 200, height: 200 }}
+      />
+      <Button
+        title={"Go to Details"}
+        onPress={() => {
+          nav.dispatch(StackActions.replace("Detail"));
+        }}
+      />
+      <Button title={"Close"} onPress={() => nav.goBack()} />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Animated.Image
+        sharedTransitionTag="image"
+        source={{ uri: "https://picsum.photos/200/300" }}
+        style={{ width: 300, height: 300 }}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Modal"
+          component={ModalScreen}
+          options={{
+            presentation: "transparentModal",
+          }}
+        />
+        <Stack.Screen name="Detail" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
